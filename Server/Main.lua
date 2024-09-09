d40ba1f5666dc8840f95da646214526a6594d1e8
Ictoan42 <christopher@mlbennett.com>
2024-09-09 20:47:22 +0100

    add config file generation to Server main

--- Server/Main.lua
+++ Server/Main.lua
@@ -1,11 +1,27 @@
-print("Running Storage System")
+print("Running Storage System..")
 
-confFilePath = "/CCStorage/Server/CCStorage.conf"
+confFilePath = "/CCStorage/Server.conf"
 
-print("Config file: " .. confFilePath)
-
-SSML = require("/CCStorage/Server/StorageSystemModemListener")
-
-SSML.main(confFilePath)
-
-print("Storage System Shut Down")
+if fs.exists(confFilePath) then
+    print("Found config file: " .. confFilePath)
+    SSML = require("/CCStorage/Server/StorageSystemModemListener")
+    SSML.main(confFilePath)
+else
+    print("There is no config file!")
+    print("Would you like to generate one now? [Y/n]")
+    while true do
+        local res = read()
+        if res == "Y" or res == "y" or res == "" then
+            print("Generating config file..")
+            fs.copy("/CCStorage/Server/ConfigTemplate.conf", confFilePath)
+            print("New config file generated at \"" .. confFilePath .. "\"")
+            print("Make sure to edit it before trying to run the server again")
+            break
+        elseif res == "N" or res == "n" then
+            print("Not generating config file.")
+            break
+        else
+            print("Unrecognised option, please try again")
+        end
+    end
+end
