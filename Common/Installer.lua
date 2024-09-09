9e2e068abab86aef0d3252538f8a8949a5cac66c
Ictoan42 <christopher@mlbennett.com>
2024-09-09 20:22:33 +0100

    add Common installer

---
+++ Common/Installer.lua
@@ -0,0 +1,24 @@
+
+-- download CCLogger (dependency)
+shell.run("wget " .. "https://raw.githubusercontent.com/Ictoan42/CCLogger/main/CCLogger.lua" .. " /CCLogger.lua")
+
+if not fs.exists("/CCStorage") then
+    error("Directory /CCStorage does not exist")
+end
+
+fs.makeDir("/CCStorage/Common")
+
+-- download main files
+local mainURL = "https://raw.githubusercontent.com/Ictoan42/CCStorage/main/"
+
+local files = {
+    "AdvancedWindowClass.lua",
+    "ButtonClass.lua",
+    "ConfigFileClass.lua",
+    "RemoteStorageSystemClass.lua",
+    "WindowManagerClass.lua"
+}
+
+for k, v in pairs(files) do
+    shell.run("wget " .. mainURL .. "Common/" .. v)
+end
