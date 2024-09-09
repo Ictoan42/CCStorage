229eb5b4265da58b913acd67e43ce6b1688b18ea
Ictoan42 <christopher@mlbennett.com>
2024-09-09 20:15:28 +0100

    rewrite installer
    now uses component specific installers

--- Installer.lua
+++ Installer.lua
@@ -1,54 +1,79 @@
--- installer for the CCStorage main storage system program
+-- Generic installer that asks which component to install then runs the relevant installer
 
--- parse args
 local args = {...}
 
-local ArgOverwrite = false
-
+local toInstall = ""
+local overwrite = false
+local helpText = [[
+Usage:
+    Installer     -> Run install wizard
+Shortcuts:
+    Installer -S  -> Install Server
+    Installer -G  -> Install Monitor_GUI
+    Installer -T  -> Install TUI
+Options:
+    -o            -> Overwrite any existing CCStorage files
+]]
 for k, v in pairs(args) do
+    if v == "-h" or v == "--help" then
+        print(helpText)
+        return
+    end
 
-    if v == "-o" then ArgOverwrite = true end
+    if     v == "-S" then toInstall = "Server"
+    elseif v == "-G" then toInstall = "Monitor_GUI"
+    elseif v == "-T" then toInstall = "TUI"
+    end
 
+    if v == "-o" then overwrite = true end
 end
 
--- safety check
-if not ArgOverwrite and fs.exists("/CCStorage") then
+term.setCursorPos(1,1)
+term.clear()
+
+if not overwrite and fs.exists("/CCStorage") then
     print("CCStorage seems to already be installed.")
-    print("Please wipe any existing files before reinstalling, or run with the '-o' flag to redownload missing files")
+    print("Please wipe any existing files before reinstalling, or run with the '-o' flag to overwrite")
     return
 end
 
--- create folder
+local installPrompt = [[
+Which component do you want to install?
+    1: Server
+    2: Monitor_GUI
+    3: TUI
+]]
+
+if toInstall == "" then --if no shortcuts set it already
+    print(installPrompt)
+
+    while toInstall == "" do
+        local response = read()
+        if     response == "1" then toInstall = "Server"
+        elseif response == "2" then toInstall = "Monitor_GUI"
+        elseif response == "3" then toInstall = "TUI"
+        else
+            print("Unrecognised value, please try again")
+        end
+    end
+end
+
+-- safety check
+if toInstall == "" then
+    print("something has gone very wrong")
+    return
+end
+
+-- actually do the installing
+
 fs.makeDir("/CCStorage")
 
--- download main files into folder
 local mainURL = "https://raw.githubusercontent.com/Ictoan42/CCStorage/main/"
 
-shell.run("wget " .. mainURL .. "ChestArrayClass.lua" .. " /CCStorage/ChestArrayClass.lua")
+shell.run("wget run " .. mainURL .. toInstall .. "/Installer.lua")
 
-shell.run("wget " .. mainURL .. "ConfigFileClass.lua" .. " /CCStorage/ConfigFileClass.lua")
-
-shell.run("wget " .. mainURL .. "ItemHandlerClass.lua" .. " /CCStorage/ItemHandlerClass.lua")
-
-shell.run("wget " .. mainURL .. "Main.lua" .. " /CCStorage/Main.lua")
-
-shell.run("wget " .. mainURL .. "SortingListClass.lua" .. " /CCStorage/SortingListClass.lua")
-
-shell.run("wget " .. mainURL .. "StorageSystemClass.lua" .. " /CCStorage/StorageSystemClass.lua")
-
-shell.run("wget " .. mainURL .. "StorageSystemModemListener.lua" .. " /CCStorage/StorageSystemModemListener.lua")
-
-shell.run("wget " .. mainURL .. "ConfigTemplate.conf" .. " /CCStorage/CCStorage.conf")
-
--- download CCLogger (dependency)
-shell.run("wget " .. "https://raw.githubusercontent.com/Ictoan42/CCLogger/main/CCLogger.lua" .. " /CCLogger.lua")
-
--- setup startup script
-term.clear()
-term.setCursorPos(1, 1)
-
-while true do
-    print("Would you like CCStorage to run on startup? [Y/n]")
+--[[ while true do
+    print("Would you like the component to run on startup? [Y/n]")
     local response = read()
 
     if response == "Y" or response == "" then
@@ -73,7 +98,9 @@ while true do
     else
         print("Unrecognised response, please re enter")
     end
-end
+end ]]
+
+-- installer for the CCStorage main storage system program
 
 print("")
 print("Installation complete")
