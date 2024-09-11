0443fad6b00a9670a4b6e971933d7bca9455d95e
Ictoan42 <christopher@mlbennett.com>
2024-09-11 01:44:59 +0100

    add annotation to ConfigFile

--- Common/ConfigFileClass.lua
+++ Common/ConfigFileClass.lua
@@ -1,11 +1,16 @@
 -- object wrapper for the storage system config file
 
+--- @class ConfigFile
+--- @field [string] string
 local ConfigFile = {}
 
 local ConfigFileMetatable = {
     __index = ConfigFile
 }
 
+--- @param filePath string
+--- @return ConfigFile
+--- Import a config file from the given path
 local function new(filePath)
 
     -- "new" basically just imports and interprets the file into a set of table entries to be accessed as a dict
@@ -20,7 +25,7 @@ local function new(filePath)
     local opts = {}
 
     for line in fileContents:gmatch("([^\n]+)") do -- iterate over everything between the "\n"s (iterate over every line)
-        
+
         if line:sub(1, 1) ~= "#" then -- "if this line is not a comment"
 
             -- find the " = " that seperates key from value
@@ -37,7 +42,7 @@ local function new(filePath)
             end
 
         end
-    
+
     end
 
     -- the "opts" table already has all the properties of the object, now just slap on the methods
