71454162f488170acbfe169e904d533e2feb635f
Ictoan42 <christopher@mlbennett.com>
2024-09-11 08:57:05 +0100

    add Result support and annotations to RemoteStorageSystem

--- Common/RemoteStorageSystemClass.lua
+++ Common/RemoteStorageSystemClass.lua
@@ -2,46 +2,112 @@
 
 -- effectively just a shim API to send standard method calls over a modem connection
 
+local R = require("/CCStorage/Common/ResultClass")
+local Ok, Err, Try, Coerce = R.Ok, R.Err, R.Try, R.Coerce
+
+--- @param response table
+--- @return Result (Result<{Result, "methodName"}>)
+--- Parses a table of format { RetValTable, "methodName"} into { Result, "methodName"}
+local function decodeResponse(response)
+    local outTab = {}
+    local convertRes = Coerce(response[1])
+    if convertRes:is_ok() then
+        outTab[1] = convertRes:unwrap()
+    else
+        return Err("Does not contain a valid result")
+    end
+    outTab[2] = response[2]
+    return Ok(outTab)
+end
+
+--- @class RemoteStorageSystem
+--- @field cfg ConfigFile
+--- @field outPort number
+--- @field inPort number
+--- @field modem modem
+--- @field blocking boolean
 local RemoteStorageSystem = {}
 
+--- @param liteMode? boolean
+--- @return Result
+--- Get a list of all items in the system. Return format is and array,
+--- in which every entry is the table returned from an individual
+--- chestPeriph.list() call. If liteMode == false, each entry also
+--- contains a chestName and chestSize entry
 function RemoteStorageSystem:list(liteMode)
     return self:sendReq({"list", liteMode})
 end
 
+--- @return Result
+--- Returns a list of every item in the system. Format is
+--- a table where t["itemID"] = itemCount
 function RemoteStorageSystem:organisedList()
     return self:sendReq({"organisedList"})
 end
 
+--- @param itemID string
+--- @return Result
+--- Finds the specified item in the system.
+--- Return format:
+--- {
+---  {chestName, slot, count, itemName},
+---  {chestName, slot, count, itemName}
+--- }
 function RemoteStorageSystem:findItems(itemID)
     return self:sendReq({"findItems", itemID})
 end
 
+--- @param inputChestID string
+--- @return Result
+--- Sort all items from the given chest into the system
 function RemoteStorageSystem:sortFromInput(inputChestID)
     return self:sendReq({"sortFromInput", inputChestID})
 end
 
+--- @param itemID string
+--- @param outputChestID string
+--- @param count? number
+--- @param toSlot? number
+--- @return Result
+--- Finds the desired item, and moves 'count' of that item
+--- to 'to'. 'count' is 64 by default.
 function RemoteStorageSystem:retrieve(itemID, outputChestID, count, toSlot)
     return self:sendReq({"retrieve", itemID, outputChestID, count, toSlot})
 end
 
+--- @return Result (number of items registered)
+--- Find all unregistered items in the system and register to the
+--- chest they were found in
 function RemoteStorageSystem:detectAndRegisterItems()
     return self:sendReq({"detectAndRegisterItems"})
 end
 
+--- @param itemID string
+--- @param chestID string
+--- @return Result (boolean)
+--- Registers the given item to the given chest
 function RemoteStorageSystem:registerItem(itemID, chestID)
     return self:sendReq({"registerItem", itemID, chestID})
 end
 
+--- @param itemID string
+--- @return Result (boolean)
+--- Unregisters the given item from the system
 function RemoteStorageSystem:forgetItem(itemID)
     return self:sendReq({"forgetItem", itemID})
 end
 
+--- @param dumpChest string
+--- @return Result
+--- Moves any unregistered items into dumpChest
 function RemoteStorageSystem:cleanUnregisteredItems(dumpChest)
     return self:sendReq({"cleanUnregisteredItems", dumpChest})
 end
 
+--- @return ConfigFile
+--- Get the system's configuration
 function RemoteStorageSystem:getConfig()
-    return self:sendReq({"getConfig"})
+    return self:sendReq({"getConfig"}):unwrap()
 end
 
 function RemoteStorageSystem:sendReq(arr)
@@ -63,7 +129,7 @@ function RemoteStorageSystem:sendBlockingReq(arr)
     end
     self.modem.transmit(self.outPort, self.inPort, arr)
     local resEv = table.pack(os.pullEvent("modem_message"))
-    return resEv[5]
+    return decodeResponse(resEv[5])
 end
 
 function RemoteStorageSystem:sendNonBlockingReq(arr)
